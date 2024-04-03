aa2da6a6375d4711566c5ca08314e03b50582039
8o9 <58972840+8o9@users.noreply.github.com>
2024-04-04 02:15:52 +0900

    add 1-8

---
+++ ch01/ex08/index.md
@@ -0,0 +1,44 @@
+# 実行内容
+- [江戸川乱歩の"怪奇四十面相"](https://www.aozora.gr.jp/cards/001779/card56673.html)をnkfでUTF-8に変換し、
+  - `cat kaiki_yonju_menso.txt|nkf -w > utf8_kaiki_yonju_menso.txt`
+- 変換プログラムを実行した
+  - `cat utf8_kaiki_yonju_menso.txt | npx node --loader ts-node/esm index.ts`
+  - Node.jsは21.6.2
+
+# 結果
+以下のように表示された。漢字が含まれる本だが、ヒストグラムには表示されていない。「、」が一番多いのは言われてみると確かにと思った
+```
+(node:11878) ExperimentalWarning: `--experimental-loader` may be removed in the future; instead use `register()`:
+--import 'data:text/javascript,import { register } from "node:module"; import { pathToFileURL } from "node:url"; register("ts-node/esm", pathToFileURL("./"));'
+(Use `node --trace-warnings ...` to show where the warning was created)
+、: ####### 7.44%
+の: ##### 4.63%
+い: ### 3.43%
+し: ### 3.11%
+た: ### 2.90%
+。: ### 2.82%
+と: ### 2.51%
+ま: ## 2.42%
+て: ## 2.42%
+か: ## 2.27%
+で: ## 2.16%
+に: ## 2.11%
+な: ## 2.07%
+が: ## 2.03%
+は: ## 1.91%
+う: ## 1.90%
+す: ## 1.80%
+っ: ## 1.64%
+を: # 1.46%
+こ: # 1.44%
+ん: # 1.42%
+り: # 1.31%
+る: # 1.29%
+も: # 1.27%
+ら: # 1.21%
+そ: # 1.18%
+く: # 1.12%
+き: # 1.11%
+れ: # 1.08%
+あ: # 1.08%
+```
--- ch01/ex08/index.ts
+++ ch01/ex08/index.ts
@@ -1,3 +1,6 @@
+// ref: "JavaScript: The Definitive Guide", v7, Japanese
+
+// Mapを拡張し、キーがマップ荷存在しない時、get()がnullの代わりにデフォルト値を返すようにする
 class DefaultMap extends Map {
   private defaultValue;
   constructor(defaultValue: number) {
@@ -8,23 +11,71 @@ class DefaultMap extends Map {
     if (this.has(key)) {
       return super.get(key);
     } else {
+      // Mapにキーがなければ独自のデフォルト値を返す
       return this.defaultValue;
     }
   }
 }
 
+// 文字とそのカウント数をもち、ASCIIアートでヒストグラムを表示するためのクラス
 class Histogram {
+  private letterCounts;
+  private totalLetters;
   constructor() {
-    this.lettersCounts = new DefaultMap(0);
+    this.letterCounts = new DefaultMap(0);
     this.totalLetters = 0;
   }
-  add(text: string) {
-    text = text.replace(/\s/g, "").toUpperCase();
 
-    for(let character of text) {
-      let count = this.letterCounts.get(character);
-      this.letterCounts.set(character, count+1);
+  // textの中の文字でヒストグラムを更新
+  add(text: string) {
+    // 空白を取り除き全て大文字に変換
+    text = text.replace(/\s/g, "").toUpperCase();
+    // 1文字ずつ、文字数をカウントしMapに追加/更新、全文字数もカウント
+    for (const character of text) {
+      const count = this.letterCounts.get(character);
+      this.letterCounts.set(character, count + 1);
       this.totalLetters++;
     }
   }
+
+  // ヒストグラムを文字列に変換し ASCIIグラフィックとして表示する
+  toString() {
+    // マップを [キー、文字数]に変換
+    let entries = [...this.letterCounts];
+    // 文字数順にソート。文字数が同じ場合はアルファベット順でソート
+    entries.sort((a, b) => {
+      if (a[1] === b[1]) {
+        return a[0] < b[0] ? -1 : 1; // アルファベット順(昇順)
+      } else {
+        return b[1] - a[1]; // 文字数の降順
+      }
+    });
+    // 文字数をパーセント(頻出度)に変換
+    for (const entry of entries) {
+      entry[1] = (entry[1] / this.totalLetters) * 100.0;
+    }
+    // 文字の頻出度1%以上を表示させる
+    entries = entries.filter((entry) => entry[1] >= 1.0);
+    // 表示用の1行のテキストに変換
+    const lines = entries.map(
+      ([l, n]) => `${l}: ${"#".repeat(Math.round(n))} ${n.toFixed(2)}%`,
+    );
+    // 各行を改行文字で区切って結合、結合した文字列を返す
+    return lines.join("\n");
+  }
 }
+
+// 標準入力から取得したテキストを非同期に読み出し、ヒストグラムに追加し返す
+async function histogramFromStdin() {
+  process.stdin.setEncoding("utf-8");
+  const histogram = new Histogram();
+  for await (const chunk of process.stdin) {
+    histogram.add(chunk);
+  }
+  return histogram;
+}
+
+// 標準入力から得られたHistogramオブジェクトを使って、ヒストグラムを表示する
+histogramFromStdin().then((histogram) => {
+  console.log(histogram.toString());
+});
