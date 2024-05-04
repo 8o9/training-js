43baaac1abb872e8eb1130dec32e3b4345bafce4
8o9 <58972840+8o9@users.noreply.github.com>
2024-05-05 05:37:23 +0900

    add codes: 4-9,10:

---
+++ ch04/ex09/index.ts
@@ -0,0 +1,6 @@
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof NaN);
+console.log(typeof 4);
+console.log(typeof console.log);
---
+++ ch04/ex10/index.ts
@@ -0,0 +1,4 @@
+const パイロット = ["p", "i", "l", "o", "t"];
+delete パイロット[3];
+console.log(パイロット);
+console.log(パイロット.length);
