8da89fe4150e107527044c969bc0231e3dfca917
8o9 <58972840+8o9@users.noreply.github.com>
2024-05-07 23:33:18 +0900

    fix format 5-2

--- ch05/ex02/index.test.ts
+++ ch05/ex02/index.test.ts
@@ -1,12 +1,14 @@
-import { replaceEscapeELIF, replaceEscapeSWITCH} from "./index.ts";
+import { replaceEscapeELIF, replaceEscapeSWITCH } from "./index.ts";
 
 describe("replaceEscapeSeq", () => {
   it("should ...", () => {
-    expect(replaceEscapeELIF(`a\0b\bc\td\ne\vf\fg\rh"i'`))
-    .toBe(`a\\0b\\bc\\td\\ne\\vf\\fg\\rh\\"i\\'`);
+    expect(replaceEscapeELIF(`a\0b\bc\td\ne\vf\fg\rh"i'`)).toBe(
+      `a\\0b\\bc\\td\\ne\\vf\\fg\\rh\\"i\\'`,
+    );
   });
   it("should ...", () => {
-    expect(replaceEscapeSWITCH(`a\0b\bc\td\ne\vf\fg\rh"i'`))
-    .toBe(`a\\0b\\bc\\td\\ne\\vf\\fg\\rh\\"i\\'`);
+    expect(replaceEscapeSWITCH(`a\0b\bc\td\ne\vf\fg\rh"i'`)).toBe(
+      `a\\0b\\bc\\td\\ne\\vf\\fg\\rh\\"i\\'`,
+    );
   });
 });
