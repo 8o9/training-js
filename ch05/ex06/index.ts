62a3c8dc05f8f8cb869c5ef8b3861c8cac0a14ff
8o9 <58972840+8o9@users.noreply.github.com>
2024-05-08 02:09:56 +0900

    5-6

---
+++ ch05/ex06/index.ts
@@ -0,0 +1,17 @@
+const tryCatchFinally = () => {
+  const str = "Buzz!";
+  try{
+    console.log(str);
+    throw new Error("bad things happened.");
+  }
+  catch (e: unknown) {
+    if (e instanceof Error) {
+      console.log(e.message);
+    }
+  }
+  finally{
+    console.log("easy come, easy go!");
+  }
+};
+
+tryCatchFinally();
