7b4d31e928468c49bf06683e5ff72b19f184afc9
8o9 <58972840+8o9@users.noreply.github.com>
2024-05-09 22:59:54 +0900

    modify how to 'for' in 5-5

--- ch05/ex05/index.ts
+++ ch05/ex05/index.ts
@@ -2,9 +2,9 @@ type strNumObj = { [key: string]: number };
 
 export const delOdds = (obj: strNumObj) => {
   let ret = {};
-  for (const k of Object.keys(obj)) {
-    if (obj[k] % 2 === 0) {
-      ret = { ...ret, [k]: obj[k] };
+  for (const [k, v] of Object.entries(obj)) {
+    if (v % 2 === 0) {
+      ret = { ...ret, [k]: v };
     }
   }
   return ret;
