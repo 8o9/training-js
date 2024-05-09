e573c9d3731357f7f31a159b6a5d81b5347d739f
8o9 <58972840+8o9@users.noreply.github.com>
2024-05-09 22:58:27 +0900

    add another func of 4-4

--- ch04/ex04/index.test.ts
+++ ch04/ex04/index.test.ts
@@ -1,6 +1,6 @@
-import { bitCount } from "./index.ts";
+import { bitCount, bitCount2 } from "./index.ts";
 
-describe("bitCount", () => {
+describe("bitCount & bitCount2", () => {
   it("should return 3 when 7(0b111) given", () => {
     expect(bitCount(0b111)).toBe(3);
   });
@@ -15,5 +15,20 @@ describe("bitCount", () => {
   });
   it("should return undefined when 0.1 given", () => {
     expect(bitCount(0.1)).toBe(undefined);
+  });
+  it("should return 3 when 7(0b111) given", () => {
+    expect(bitCount2(0b111)).toBe(3);
+  });
+  it("should return 2 when 5(0b101) given", () => {
+    expect(bitCount2(0b101)).toBe(2);
+  });
+  it("should return 32 when -1(0b1111...1) given", () => {
+    expect(bitCount2(-1)).toBe(32);
+  });
+  it("should return 0 when 0 given", () => {
+    expect(bitCount2(0)).toBe(0);
+  });
+  it("should return undefined when 0.1 given", () => {
+    expect(bitCount2(0.1)).toBe(undefined);
   });
 });
--- ch04/ex04/index.ts
+++ ch04/ex04/index.ts
@@ -3,11 +3,9 @@ export const bitCount = (num: number): number | undefined => {
   if (Number.isSafeInteger(num) === false) {
     return undefined;
   }
-  // 負値の場合、>>>を用いて2の補数表現にする
-  // (-1).toString(2)が-1になるので
-  if (num < 0) {
-    num = num >>> 0;
-  }
+  // >>>を用い0埋め32bitにする
+  // numが負数の時、例えば(-1).toString(2)が-1になるので
+  num >>>= 0;
   // 2進数表現の文字列に変換し、1文字ずつの配列に変換、
   // 配列の要素が1の場合にcountを+1(短絡評価で&&の前がtrueの時に&&の後が実行)
   let count = 0;
@@ -15,5 +13,19 @@ export const bitCount = (num: number): number | undefined => {
     .toString(2)
     .split("")
     .forEach((elm) => elm === "1" && count++);
+  return count;
+};
+
+export const bitCount2 = (num: number): number | undefined => {
+  // 整数でない場合はundefined
+  if (Number.isSafeInteger(num) === false) {
+    return undefined;
+  }
+  let count = 0;
+  num >>>= 0;
+  while (num !== 0) {
+    count += num & 1;
+    num >>>= 1;
+  }
   return count;
 };
