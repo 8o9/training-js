b387961625d6eecc58b474e641338a81c88fc35f
8o9 <58972840+8o9@users.noreply.github.com>
2024-06-05 08:56:44 +0900

    fix 6-5

--- ch06/ex05/index.ts
+++ ch06/ex05/index.ts
@@ -1,6 +1,6 @@
 const prototypeObject = {
   36: "数値名プロパティ名", // "36"でも同じ?
-  "name": "文字列名プロパティ"
+  name: "文字列名プロパティ",
 };
 
 const obj = Object.create(prototypeObject);
@@ -12,7 +12,7 @@ obj[36] = "プロトタイプと同名の数値名プロパティ";
 obj["name"] = "プロトタイプと同名の文字列名プロパティ";
 
 Object.defineProperty(obj, "name", {
-  enumerable: false
+  enumerable: false,
 });
 
 for (const key in obj) {
@@ -23,7 +23,7 @@ for (const key in obj) {
 // name2, 新しい文字列名プロパティ
 //// nameは列挙/表示されない
 
-console.log('-----');
+console.log("-----");
 
 for (const key of Reflect.ownKeys(obj)) {
   console.log(`${String(key)}, ${obj[key]}`);
