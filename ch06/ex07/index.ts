ef1106de7627435a0e309f07176606f93d14504f
8o9 <58972840+8o9@users.noreply.github.com>
2024-06-17 23:45:50 +0900

    fix 6-7

--- ch06/ex07/index.ts
+++ ch06/ex07/index.ts
@@ -4,17 +4,21 @@
 // Record<string, unknown>はオブジェクトを表す便利な型らしい. こうしないとtarget[p]のところでlintに怒られる
 //   https://typescriptbook.jp/reference/type-reuse/utility-types/record
 export const assign = (
-  target: Record<string, unknown>,
-  ...sources: Record<string, unknown>[]
+  target: Record<string | symbol, unknown>,
+  ...sources: Record<string | symbol, unknown>[]
 ) => {
   for (const source of sources) {
     // 列挙可能なプロパティ全てについてコピーするが、
     for (const p in source) {
       // 継承プロパティは除きたい。自身のプロパティのみコピーする
-      // Symbolも
       if (Object.prototype.hasOwnProperty.call(source, p)) {
         target[p] = source[p];
       }
+    }
+    // Symbolも
+    const symbolKeys = Object.getOwnPropertySymbols(source);
+    for (const key of symbolKeys) {
+      target[key] = source[key];
     }
   }
   return target;
