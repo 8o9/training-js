694cce93a9eeb8955666b3e1593f649cf3c23019
8o9 <58972840+8o9@users.noreply.github.com>
2024-07-05 02:46:35 +0900

    fix 9-5 format

--- ch09/ex05/index.test.ts
+++ ch09/ex05/index.test.ts
@@ -1,14 +1,14 @@
-import { instanceOf } from './index.ts';
+import { instanceOf } from "./index.ts";
 
-describe('instanceOf function', () => {
-  it('should handle non-class constructor', () => {
+describe("instanceOf function", () => {
+  it("should handle non-class constructor", () => {
     // function TestConstructor() {} // ts2345 error
     class TestConstructor {}
     const testObject = Object.create(TestConstructor.prototype);
     expect(instanceOf(testObject, TestConstructor)).toBe(true);
   });
 
-  it('should return true for 多重/多段 inheritance', () => {
+  it("should return true for 多重/多段 inheritance", () => {
     class BaseClass {}
     class SecondClass extends BaseClass {}
     class ThirdClass extends SecondClass {}
@@ -16,7 +16,7 @@ describe('instanceOf function', () => {
     expect(instanceOf(testObject, BaseClass)).toBe(true);
   });
 
-  it('should return false for 継承関係にない classes', () => {
+  it("should return false for 継承関係にない classes", () => {
     class ClassA {}
     class ClassB {}
     const testObject = new ClassA();
