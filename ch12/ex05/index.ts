2f0d01b479884680c15112c3a9c667d84653ac09
8o9 <58972840+8o9@users.noreply.github.com>
2024-09-06 09:57:41 +0900

    WIP add 12-5

--- ch12/ex05/index.ts
+++ ch12/ex05/index.ts
@@ -1,3 +1,23 @@
 import fs from "fs";
+import {Buffer} from 'buffer'
 
-export function* readLines(filePath: string): Generator<string> {}
+// very, very, WIP
+export function* readLines(filePath: string): Generator<string> {
+  
+  const startPos = 0;
+  const readLength = 1024;
+  let readBuffer = Buffer.alloc(readLength);
+  let fileDesc;
+  let beforeLineBreak = '';
+
+  try {
+    fileDesc = fs.openSync(filePath, 'r');
+    let readBytes;
+    while(readBytes = fs.readSync(fileDesc, readBuffer, 0, readLength, startPos) > 0) {
+      let data = beforeLineBreak + readBuffer.toString('utf-8', 0, readBytes);
+    }
+
+  } finally {
+    fs.closeSync(fileDesc);
+  }
+}
