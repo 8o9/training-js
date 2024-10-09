b7ce04b58bf85aeafdacaaad8e81e1e0a0a13073
8o9 <58972840+8o9@users.noreply.github.com>
2024-10-09 23:47:49 +0900

    fix 15.01-03.06

--- ch15.01-03/ex06/index.js
+++ ch15.01-03/ex06/index.js
@@ -9,4 +9,28 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("ip-link").href = "https://ipinfo.io/" + ipAddr;
     })
     .catch((error) => console.error("Error fetching IP info ", error));
+  document.querySelector("#your-OS").textContent =
+    navigator.userAgentData.platform;
+  document.querySelector("#your-browser").textContent =
+    navigator.userAgentData.brands[0].brand;
+  const opt = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  const suc = (pos) => {
+    const crd = pos.coords;
+    document.querySelector(
+      "#your-pos",
+    ).textContent = `${crd.latitude} ${crd.longitude}`;
+    document.querySelector(
+      "#pos-link",
+    ).href = `https://www.google.com/maps/search/?api=1&query=${crd.latitude},${crd.longitude}`;
+  };
+
+  const err = (err) => {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
+  navigator.geolocation.getCurrentPosition(suc, err, opt);
 });
