6e10369645cd804cb8c89a878df95beb1a0865c3
8o9 <58972840+8o9@users.noreply.github.com>
2025-01-09 04:26:32 +0900

    fix format

--- ch16/ex06/index.js
+++ ch16/ex06/index.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
+import fs from "fs";
 
-fs.truncate('example.txt', 100, err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('File truncated. All OK!');
-    }
+fs.truncate("example.txt", 100, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("File truncated. All OK!");
+  }
 });
